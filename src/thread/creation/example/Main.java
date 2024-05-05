7eb31bedd616dd27825753f988dab3d032b03c16
jeong <chung_junhuck@infonic.co.jp>
2024-05-06 03:17:16 +0900

    handle exception

--- src/thread/creation/example/Main.java
+++ src/thread/creation/example/Main.java
@@ -5,19 +5,19 @@ public class Main {
             @Override
             public void run() {
                 // Code that will run in a new thread
-                System.out.println("We are now in thread " + Thread.currentThread().getName());
-                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+                throw new RuntimeException("Intentional Exception");
             }
         });
+        
+        thread.setName("Misbehaving thread");
 
-        thread.setName("New Worker Thread");
-
-        thread.setPriority(Thread.MAX_PRIORITY);
-
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A Critical error happened in thread " + t.getName()
+                + " the error is " + e.getMessage());
+            }
+        });
         thread.start();
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
-
-        Thread.sleep(10000);
     }
 }
