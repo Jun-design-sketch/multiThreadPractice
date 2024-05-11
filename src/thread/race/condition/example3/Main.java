495cab7e84e58c8cf253e93a8b4f80372e6d8913
jeong <chung_junhuck@infonic.co.jp>
2024-05-11 15:58:51 +0900

    race end

---
+++ src/thread/race/condition/example3/Main.java
@@ -0,0 +1,72 @@
+package thread.race.condition.example3;
+
+import java.io.ObjectStreamException;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+
+    }
+
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+        Object lock = new Object();
+        public void increment() {
+            // MEMO: ++,-- is not a atomic operation
+            // 1. currentValue = 0
+            // 2. newValue = currentValue + 1 = 1
+            // 3. items = newValue = 1
+            synchronized (this.lock) {
+                items++;
+            }
+        }
+        public void decrement() {
+            synchronized (this.lock) {
+                items--;
+            }
+        }
+        public int getItems() {
+            synchronized (this.lock) {
+                return items;
+            }
+        }
+    }
+}
+
