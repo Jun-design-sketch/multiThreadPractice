b41bdc1d32f504950fbc419395b7b5beccc53a15
jeong <chung_junhuck@infonic.co.jp>
2024-05-12 22:33:56 +0900

    add volatile

---
+++ src/thread/non/atomic/operation/example1/Main.java
@@ -0,0 +1,38 @@
+package thread.non.atomic.operation.example1;
+
+public class Main {
+    public static void main(String [] args) {
+        SharedClass sharedClass = new SharedClass();
+        Thread thread1 = new Thread(() -> {
+           for(int i = 0; i < Integer.MAX_VALUE; i++) {
+               sharedClass.increment();
+           }
+        });
+
+        Thread thread2 = new Thread(() -> {
+           for(int i = 0; i < Integer.MAX_VALUE; i++) {
+               sharedClass.checkForDataRace();
+           }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    public static class SharedClass {
+        private volatile int x = 0;
+        private volatile int y = 0;
+
+        public void increment() {
+            // MEMO: no dependency between 2 lines code -> Data Race occur
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace() {
+            if(y > x) {
+                System.out.println("y > x : Data Race is detected");
+            }
+        }
+    }
+}
