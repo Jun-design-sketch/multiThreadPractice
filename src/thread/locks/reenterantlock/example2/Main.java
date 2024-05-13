7c10810fa7cca309b818658bbcf9deeec4860fa9
jeong <jeong.junhueck@gmail.com>
2024-05-14 01:04:05 +0900

    prepare InventoryDatabase: for read/write lock

---
+++ src/thread/locks/reenterantlock/example2/Main.java
@@ -0,0 +1,49 @@
+package thread.locks.reenterantlock.example2;
+
+import com.sun.source.tree.Tree;
+
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static class InventoryDatabase {
+        private TreeMap<Integer, Integer> priceToCountMap = new TreeMap<>();
+
+        public int getNumberOfItemsInPriceRange(int lowerBound, int upperBound){
+            Integer fromKey = priceToCountMap.ceilingKey(lowerBound);
+            Integer toKey = priceToCountMap.floorKey(upperBound);
+            if ( fromKey == null || toKey == null ) {
+                return 0;
+            }
+            NavigableMap<Integer, Integer> rangeOfPrices = priceToCountMap.subMap(fromKey, true, toKey, true);
+
+            int sum = 0;
+            for (int numberOfItemsForPrice : rangeOfPrices.values()) {
+                sum += numberOfItemsForPrice;
+            }
+            return sum;
+        }
+
+        public void addItem(int price){
+            Integer numberOfItemsForPrice = priceToCountMap.get(price);
+            if (numberOfItemsForPrice == null ) {
+                priceToCountMap.put(price, 1);
+            } else {
+                priceToCountMap.put(price, numberOfItemsForPrice + 1);
+            }
+        }
+
+        public void removeItem(int price){
+            Integer numberOfItemsForPrice = priceToCountMap.get(price);
+            if (numberOfItemsForPrice == null || numberOfItemsForPrice == 1) {
+                priceToCountMap.remove(price);
+            } else {
+                priceToCountMap.put(price, numberOfItemsForPrice - 1);
+            }
+        }
+    }
+}
